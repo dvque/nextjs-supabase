84f4ea7b08b2b4c69d2f1bebdaa85c3fe8a51e06
dvque <dvdquerol@gmail.com>
2024-07-23 18:57:06 +0200

    chore: Add login page component
    The login page component has been added to the project. This component handles user login functionality, including capturing user input, making API calls to authenticate the user, and updating the UI accordingly. This addition enhances the application's user authentication capabilities.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+
+export default function Login() {
+
+    const [data, setData] = useState<{
+        email: string,
+        password: string
+    }>({
+        email: '',
+        password: ''
+    })
+
+
+
+    const login = async () => {
+        console.log('data', data)
+        try {
+            let { data: dataUser, error } = await supabase.auth.signUp({
+                email: data.email,
+                password: data.password
+            })
+
+            if (data) {
+                console.log('data', data)
+            }
+        }
+        catch (error) {
+            console.log('error', error)
+        }
+
+    }
+
+    const handleChange = (e: any) => {
+
+        const { name, value } = e.target;
+
+        setData((prev: any) => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+
+    return <div className="container mx-auto w-[400px]">
+        <h1>Login</h1>
+        <div className='grid'>
+            <label>Email</label>
+            <input
+                type="text"
+                name="email"
+                value={data?.email}
+                onChange={handleChange}
+                style={{ color: 'black' }}
+            />
+        </div>
+        <div className='grid'>
+            <label>Password</label>
+            <input
+                type="password"
+                name="password"
+                value={data?.password}
+                onChange={handleChange}
+                style={{ color: 'black' }}
+            />
+        </div>
+        <div><button onClick={login}>Signup</button></div>
+    </div>
+}
